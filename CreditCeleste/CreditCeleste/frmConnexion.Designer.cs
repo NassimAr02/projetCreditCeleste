43016212c792238f8c64952af4063e8e1a5c697d
NassimAr02 <nassim.arrasspro@gmail.com>
2024-11-20 08:53:11 +0100

    Création de la page frmAccueil

---
+++ CreditCeleste/CreditCeleste/frmConnexion.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace CreditCeleste
+{
+    partial class frmConnexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValiderConnexion = new System.Windows.Forms.Button();
+            this.txtUtilisateur = new System.Windows.Forms.TextBox();
+            this.txtMdp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnValiderConnexion
+            // 
+            this.btnValiderConnexion.Location = new System.Drawing.Point(33, 170);
+            this.btnValiderConnexion.Name = "btnValiderConnexion";
+            this.btnValiderConnexion.Size = new System.Drawing.Size(126, 36);
+            this.btnValiderConnexion.TabIndex = 0;
+            this.btnValiderConnexion.Text = "Valider";
+            this.btnValiderConnexion.UseVisualStyleBackColor = true;
+            // 
+            // txtUtilisateur
+            // 
+            this.txtUtilisateur.Location = new System.Drawing.Point(33, 43);
+            this.txtUtilisateur.Name = "txtUtilisateur";
+            this.txtUtilisateur.Size = new System.Drawing.Size(126, 22);
+            this.txtUtilisateur.TabIndex = 1;
+            // 
+            // txtMdp
+            // 
+            this.txtMdp.Location = new System.Drawing.Point(33, 105);
+            this.txtMdp.Name = "txtMdp";
+            this.txtMdp.Size = new System.Drawing.Size(126, 22);
+            this.txtMdp.TabIndex = 2;
+            this.txtMdp.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nom d\'utilisateur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mot de passe";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUtilisateur);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnValiderConnexion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMdp);
+            this.groupBox1.Location = new System.Drawing.Point(283, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 249);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connexion";
+            // 
+            // frmConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmConnexion";
+            this.Text = "frmConnexion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValiderConnexion;
+        private System.Windows.Forms.TextBox txtUtilisateur;
+        private System.Windows.Forms.TextBox txtMdp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
