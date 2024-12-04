bc1f2fb5608bb77cfd02fc21ab51eae0d62f9c0c
NassimAr02 <nassim.arrasspro@gmail.com>
2024-12-04 15:45:19 +0100

    Classe facture2

--- CreditCeleste/CreditCeleste/Facture.cs
+++ CreditCeleste/CreditCeleste/Facture.cs
@@ -8,9 +8,42 @@ namespace CreditCeleste
 {
     class Facture
     {
-        private string dateFacture;
+        private string dateFacture ;
         private string typeFrais;
         private double montantF;
 
+        public Facture (string aaaammjj,string type,double montant)
+        {
+            dateFacture = aaaammjj;
+            typeFrais = type;
+            montantF = montant;
+        }
+
+        public void setDateFacture(string aaaammjj)
+        {
+            dateFacture = aaaammjj;
+
+        }
+        public string getDataFacture()
+        {
+            return dateFacture;
+        }
+        public void setTypeFrais(string type)
+        {
+            typeFrais = type;
+        }
+        public string getTypeFrais()
+        {
+            return typeFrais;
+        }
+        public void setMontantF(double montant)
+        {
+            montantF = montant;
+        }
+
+        public double getMontantF()
+        {
+            return montantF;
+        }
     }
 }
