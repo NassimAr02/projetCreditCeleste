8a15fd8e7a216e7ea4302217b2c174a60e7a464b
AlbProDev <albertgadaev.pro@gmail.com>
2025-04-02 16:46:29 +0200

    Modif de la page compta

--- CreditCeleste/CreditCeleste/frmComptabilité.Designer.cs
+++ CreditCeleste/CreditCeleste/frmComptabilité.Designer.cs
@@ -29,31 +29,47 @@ namespace CreditCeleste
         /// </summary>
         private void InitializeComponent()
         {
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lstBoxFactures = new System.Windows.Forms.ListBox();
+            this.btnBDDfill = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // dateTimePicker1
+            // lstBoxFactures
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(0, 0);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 0;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.lstBoxFactures.FormattingEnabled = true;
+            this.lstBoxFactures.ItemHeight = 16;
+            this.lstBoxFactures.Location = new System.Drawing.Point(147, 123);
+            this.lstBoxFactures.Name = "lstBoxFactures";
+            this.lstBoxFactures.Size = new System.Drawing.Size(400, 212);
+            this.lstBoxFactures.TabIndex = 0;
+            this.lstBoxFactures.SelectedIndexChanged += new System.EventHandler(this.lstBoxFactures_SelectedIndexChanged);
+            // 
+            // btnBDDfill
+            // 
+            this.btnBDDfill.Location = new System.Drawing.Point(628, 143);
+            this.btnBDDfill.Name = "btnBDDfill";
+            this.btnBDDfill.Size = new System.Drawing.Size(75, 23);
+            this.btnBDDfill.TabIndex = 1;
+            this.btnBDDfill.Text = "Remplir lstbox";
+            this.btnBDDfill.UseVisualStyleBackColor = true;
+            this.btnBDDfill.Click += new System.EventHandler(this.btnBDDfill_Click);
             // 
             // frmComptabilite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnBDDfill);
+            this.Controls.Add(this.lstBoxFactures);
             this.Name = "frmComptabilite";
             this.Text = "Comptabilité";
+            this.Load += new System.EventHandler(this.frmComptabilite_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListBox lstBoxFactures;
+        private System.Windows.Forms.Button btnBDDfill;
     }
 }
--- CreditCeleste/CreditCeleste/frmComptabilité.cs
+++ CreditCeleste/CreditCeleste/frmComptabilité.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CreditCeleste
 {
     public partial class frmComptabilite : Form
     {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
         public frmComptabilite()
         {
             InitializeComponent();
@@ -19,6 +25,33 @@ namespace CreditCeleste
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void frmComptabilite_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstBoxFactures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBDDfill_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source = localhost\\SQLEXPRESS; Integrated Security =SSPI; Initial Catalog=creditCelesteARRASS");
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM Factures";
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                lstBoxFactures.Items.Add(dr["typeFrais"]);
+            }
+            con.Close();
 
         }
     }
